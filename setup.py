a1ef1142daa2e13e577bfbeb1e2db0e46a5dd583
Hurin Hu <hurin@live.ca>
2021-05-10 17:02:19 +1200

    Update setup.py
    Fixed Geo2Array error

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PointInPolygon",
-    version="1.0.1",
+    version="1.0.2",
     author="Hurin Hu",
     author_email="hurin@live.ca",
     description="Check point in polygon for Python",
